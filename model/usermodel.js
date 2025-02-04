47aab7b2764c149d91a5faf336038f1630c9fc36
MahekPaghdal <paghdalmahek0@gmail.com>
2025-02-04 14:23:14 -0800

    message

---
+++ model/usermodel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var userschema = new mongoose.Schema({
+    name:{
+        type :String
+    },
+    email:{
+        type :String
+    },
+    password:{
+        type :String
+    }
+});
+module.exports = mongoose.model("mahek",userschema);
