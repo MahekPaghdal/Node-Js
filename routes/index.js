bde8f9f80924ab5b82ca9ef9a12cfb3478db5457
MahekPaghdal <paghdalmahek0@gmail.com>
2025-02-06 15:13:37 -0800

    success

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ var user = require('../controller/usercontroller');
 /* GET home page. */
 
 router.post('/',user.insert);
-router.get('/',user.getdata);
+router.get('/get',user.getdata);
 router.get('/update/:id',user.get_update_data);
 router.post('/update/:id',user.update_data);
 router.get('/delete/:id',user.delete_data);
+router.post('/login',user.login);
+
 
 module.exports = router;
