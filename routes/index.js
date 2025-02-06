4d831df38e1512037957e0df310a0a0a823648e3
MahekPaghdal <paghdalmahek0@gmail.com>
2025-02-06 15:44:56 -0800

    msg chnage

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var user = require('../controller/usercontroller');
 
 /* GET home page. */
 
-router.post('/',user.insert);
-router.get('/get',user.getdata);
+router.post('/adddata',user.insert);
+router.get('/getdata',user.getdata);
 router.get('/update/:id',user.get_update_data);
 router.post('/update/:id',user.update_data);
 router.get('/delete/:id',user.delete_data);
